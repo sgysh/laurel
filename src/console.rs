f334be8d80a157888ae0a4e958399300f6ef9425
y-sugino <ysgn0101@gmail.com>
2018-05-04 10:17:15 +0900

    Support a built-in command

--- src/console.rs
+++ src/console.rs
@@ -1,9 +1,12 @@
+use alloc::string::String;
 use alloc::vec_deque::VecDeque;
+use core;
 use sched;
 use uart;
 
 pub fn console_handler() {
     let mut buffer: VecDeque<u32> = VecDeque::with_capacity(10);
+    let mut command: String = String::with_capacity(10);
     push_back_data(&mut buffer, "> ");
 
     loop {
@@ -11,9 +14,11 @@ pub fn console_handler() {
             if uart::is_readable() {
                 let val = uart::read();
                 if val == '\r' as u32 {
+                    command = run_command(command);
                     push_back_data(&mut buffer, "\r\n> ");
                 } else {
                     buffer.push_back(val);
+                    command.push(core::char::from_u32(val).unwrap());
                 }
             } else if uart::is_writeable() && !buffer.is_empty() {
                 uart::write(buffer.pop_front().unwrap());
@@ -27,5 +32,34 @@ pub fn console_handler() {
 fn push_back_data(buffer: &mut VecDeque<u32>, data: &str) {
     for c in data.chars() {
         buffer.push_back(c as u32);
+    }
+}
+
+fn run_command(mut command: String) -> String {
+    if !command.is_empty() {
+        if command == ":" {
+            // do nothing
+        } else {
+            write_console("\r\n");
+            write_console("command not found");
+        }
+        command.clear()
+    }
+
+    command
+}
+
+pub fn write_console(data: &str) {
+    for c in data.chars() {
+        loop {
+            unsafe {
+                if uart::is_writeable() {
+                    uart::write(c as u32);
+                    break;
+                } else {
+                    sched::task_switch();
+                }
+            }
+        }
     }
 }
